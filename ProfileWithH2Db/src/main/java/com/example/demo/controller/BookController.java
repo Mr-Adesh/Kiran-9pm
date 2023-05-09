4ba6030e8de63c9a48e3d95ccdcb9f8f50b10122
Mr-Adesh <36359386+Mr-Adesh@users.noreply.github.com>
2023-05-09 21:48:26 +0530

    Spring profile-09-05-2023

---
+++ ProfileWithH2Db/src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
+import com.example.demo.service.BookService;
+
+@RestController
+public class BookController {
+	@Autowired
+	BookService bookService;
+	@PostMapping("/book")
+	public Book save(@RequestBody Book book) {
+		return bookService.save(book);
+	}
+
+}
---
+++ ProfileWithH2Db/src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Book;
+import com.example.demo.repository.BookRepo;
+
+@Service
+public class BookService {
+	@Autowired
+	BookRepo bookRepo;
+public Book save(Book book) {
+	return bookRepo.save(book);
+	} 
+}
